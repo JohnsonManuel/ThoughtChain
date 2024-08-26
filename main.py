1fc364534aeeba4f1903ddcf256e54d2d88cabd6
Johnson Manuel <johnsonmanuel321@gmail.com>
2024-08-26 18:52:26 +0200

    Used Hugging Face Transformers - for models google flan t5 large used.

--- main.py
+++ main.py
@@ -2,33 +2,55 @@ import streamlit as st
 import youtube_helper as yth
 import textwrap
 
-st.title("YouTube Assistant")
+if st.secrets["HUGGINGFACEHUB_API_TOKEN"] != "" :
+    st.session_state["api_key"] = st.secrets["HUGGINGFACEHUB_API_TOKEN"]
 
+if 'api_key' not in st.session_state:
+    st.session_state['api_key'] = None
+
+# Sidebar content
 with st.sidebar:
-    with st.form(key='my_form'):
-        youtube_url = st.sidebar.text_area(
-            label="What is the YouTube video URL?",
-            max_chars=50
-            )
-        query = st.sidebar.text_area(
-            label="Ask me about the video?",
-            max_chars=50,
-            key="query"
-            )
-        # openai_api_key = st.sidebar.text_input(
-        #     label="OpenAI API Key",
-        #     key="langchain_search_api_key_openai",
-        #     max_chars=50,
-        #     type="password"
-        #     )
-        # submit_button = st.form_submit_button(label='Submit')
+    if st.session_state['api_key'] is None:
+        # First form to input API key
+        form1 = st.form(key="api_key_form")
+        with form1:
+            st.title("Enter API Key")
+            api_key_input = st.text_input("API Key", type="password")
+            submit_button = st.form_submit_button("Submit")
 
-if query and youtube_url:
-    # if not openai_api_key:
-    #     st.info("Please add your OpenAI API key to continue.")
-    #     st.stop()
-    # else:
-        db = yth.create_db_from_youtube_video_url(youtube_url)
-        response, docs = yth.get_response_from_query(db, query)
-        st.subheader("Answer:")
-        st.text(textwrap.fill(response, width=85))
+            # Save the API key if the form is submitted
+            if submit_button and api_key_input != "":
+                st.session_state['api_key'] = api_key_input
+                st.rerun()
+
+    else:
+        # Display new form once API key is entered
+        with st.form(key='secondary_form'):
+            st.title("Ask questions on any youtube video!")
+            
+            youtube_url = st.text_area(
+                label="What is the YouTube video URL?",
+            )
+            query = st.text_area(
+                label="Ask me about the video?",
+                key="query"
+            )
+            submit_button = st.form_submit_button("Submit")
+
+            if submit_button and youtube_url and query:
+                db = yth.create_db_from_youtube_video_url(youtube_url)
+                response, docs = yth.get_response_from_query(db, query, st.session_state['api_key'])
+                st.session_state['response'] = response
+
+# Main page content
+st.title("Youtube Assistant")
+
+if st.session_state['api_key']:
+    st.sidebar.success("Your API key will be used for the App.")
+else:
+    st.sidebar.warning("Please enter your Hugging Face API key to continue.")
+
+# Display the answer and other details in the main body
+if 'response' in st.session_state:
+    st.subheader("Answer:")
+    st.text(textwrap.fill(st.session_state['response'], width=85))
--- youtube_helper.py
+++ youtube_helper.py
@@ -1,58 +1,79 @@
-from langchain.document_loaders import YoutubeLoader
+from transformers import AutoConfig
+from langchain_community.document_loaders import YoutubeLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import HuggingFaceEmbeddings  # Use Hugging Face for embeddings
-from langchain.vectorstores import FAISS
-from langchain import HuggingFaceHub
-from langchain import PromptTemplate
-from langchain.chains import LLMChain
-from dotenv import load_dotenv
+from langchain_huggingface import HuggingFaceEmbeddings  # Use Hugging Face for embeddings
+from langchain_community.vectorstores import FAISS
+from langchain_community.llms import HuggingFaceHub
+from langchain_core.prompts import PromptTemplate
+from langchain_huggingface import HuggingFaceEmbeddings
+import re
+import requests
 
-load_dotenv()
-
-# Replace OpenAI embeddings with Hugging Face embeddings
+repo_id = "google/flan-t5-large"
 embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
 def create_db_from_youtube_video_url(video_url: str) -> FAISS:
-    loader = YoutubeLoader.from_youtube_url(video_url)
+    loader = YoutubeLoader.from_youtube_url(video_url,add_video_info =True)
     transcript = loader.load()
-
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=512, chunk_overlap=0)
     docs = text_splitter.split_documents(transcript)
-
-    db = FAISS.from_documents(docs, embeddings)
+    db = FAISS.from_documents(docs,embeddings)
     return db
 
-def get_response_from_query(db, query, k=4):
+def validate_api_key(api_key):
+    if (api_key == None or api_key == "" ):
+        return False
+    url = "https://huggingface.co/api/whoami-v2" 
+    headers = {
+        "Authorization": f"Bearer {api_key}"
+    }
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return True  
+    else:
+        return False
+
+def get_response_from_query(db, query,hf_access_token,k=4):
     """
     Handles a query by searching for the most similar documents in the vector store and
     generating a response using a language model.
     """
     docs = db.similarity_search(query, k=k)
+
+    # docs_page_content = " ".join([f"Title : {d.metadata['title']} Content : {d.page_content}" for d in docs])
     docs_page_content = " ".join([d.page_content for d in docs])
 
+    #  Cleaning the context to reduce the size
+    docs_page_content = text_cleaning(docs_page_content)
     # Initialize the language model from Hugging Face Hub
-    llm = HuggingFaceHub(repo_id="gpt2", model_kwargs={"temperature": 0.7, "max_new_tokens": 50})
+    llm = HuggingFaceHub(repo_id=repo_id,
+                         model_kwargs={"temperature":0.8, 'max_new_tokens' : 250,
+                                          },huggingfacehub_api_token=hf_access_token)
 
     prompt = PromptTemplate(
         input_variables=["question", "docs"],
         template="""
-        You are a helpful assistant that can answer questions about YouTube videos 
-        based on the video’s transcript.
+        You are a knowledgeable assistant. Based on the provided context, answer the following question in as much detail as possible. If the context does not allow for a long answer, provide a concise yet informative response.
+        Context: {docs}
 
-        Answer the following question: {question}
-        By searching the following video transcript: {docs}
-
-        Only use the factual information from the transcript to answer the question.
-
-        If you feel like you don't have enough information to answer the question, say "I don't know".
-
-        Your answers should be verbose and detailed.
+        Question: {question}
         """,
     )
-
-    chain = LLMChain(llm=llm, prompt=prompt)
-
+    chain = prompt | llm
     # Generate the response
-    response = chain.run(question=query, docs=docs_page_content)
+    print(prompt.format(question=query, docs=docs_page_content))
+    response = chain.invoke({"question":query , "docs":docs_page_content})
     response = response.replace("\n", "")
     return response, docs
+
+def text_cleaning(text):
+    text = re.sub(r'\d+:\d+', '', text)  # Remove timestamps
+    text = re.sub(r'\b[A-Z][A-Z0-9]*:\s*', '', text)  # Remove speaker labels
+    # Remove URLs and email addresses
+    text = re.sub(r'http\S+|www\S+|https\S+', '', text, flags=re.MULTILINE)  # Remove URLs
+    text = re.sub(r'\S+@\S+', '', text)  # Remove email addresses
+    # Lowering them for better readability
+    text = text.lower()
+    
+    return text
+
